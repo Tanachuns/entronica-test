eb4408d11b931aefe3b89cc262e50ea2d6f21938
Tanarak Chunsanit <tanarak.chuns@gmail.com>
2024-06-12 15:25:41 +0700

    init java

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hellowrold");
+    }
+
+    public static ArrayList<Integer> intersectionArrays() {
+        return new ArrayList<Integer>();
+    }
+
+    public static ArrayList<Integer> sortNumbers() {
+        return new ArrayList<Integer>();
+    }
+};
